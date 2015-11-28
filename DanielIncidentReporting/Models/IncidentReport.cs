eafce43c047fb9b495b92778e8875f5c4ff263d5
gchinfatt <N00980280@ospreys.unf.edu>
2015-11-28 14:18:10 -0500

    Initial commit

---
+++ DanielIncidentReporting/Models/IncidentReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace DanielIncidentReporting.Models
+{
+    public class IncidentReport
+    {
+        public int id { get; set; }
+        public  DateTime incidentDate { get; set; }
+        public string programName { get; set; }
+        public string location { get; set; }
+        public string incidentType { get; set; }
+        public string reporterFirstName { get; set; }
+        public string reporterLastName { get; set; }
+        public string Description { get; set; }        
+    }
+
+    public class IRTSDBContext : DbContext
+    {
+        public DbSet<IncidentReport> IncidentReports { get; set; }
+    }
+}
