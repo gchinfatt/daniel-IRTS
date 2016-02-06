f7a5c33ed4eee16e7738ae45be35722e509aefd0
gchinfatt <N00980280@ospreys.unf.edu>
2016-02-06 11:48:21 -0500

    Changed 'p' to 'P' on Confirm Password in AccountViewModel

--- DanielIncidentReporting/Models/AccountViewModels.cs
+++ DanielIncidentReporting/Models/AccountViewModels.cs
@@ -76,7 +76,7 @@ namespace DanielIncidentReporting.Models
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirm Password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
